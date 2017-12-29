6fc93afb0eee664e87ace684e00504ea54c8b8c5
singlakaran <karansingla120@gmail.com>
2017-12-29 08:31:53 +0530

    java paint

---
+++ java/java/src/PaintApp.java
@@ -0,0 +1,22 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public class PaintApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("paint.fxml"))));
+        stage.setTitle("Paint App");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
